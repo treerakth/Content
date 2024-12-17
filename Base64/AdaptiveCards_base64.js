522a52e973b7a9a21b071e39c05c3acea203433e
Treerak Taidecha <68236421+treerakth@users.noreply.github.com>
2024-12-17 07:59:04 +0700

    Update AdaptiveCards_base64.js

--- Base64/AdaptiveCards_base64.js
+++ Base64/AdaptiveCards_base64.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs");
 
 // URL ของ Webhook จาก Microsoft Teams
-const WEBHOOK_URL = "https://nu365.webhook.office.com/webhookb2/efb3b6cb-58c9-4b21-a4e3-c34adfa86609@bf1eb3f8-19d2-409d-b0c5-4e80c943fd52/IncomingWebhook/9bf57115d7a3478d83155dfb091b57f7/395194ba-631a-4986-8e13-827d36d58e85/V2p9-r_PPqfG3_boA8VcG_PadiaucvOh73hF5LvKVRVCQ1";
+const WEBHOOK_URL = "https://nu365.webhook.office.com/webhookb2/YOU_WEB_HOOK";
 
 async function sendMessageToTeamsWithBase64(imagePath, imageDetail) {
     try {
@@ -63,7 +63,7 @@ async function sendMessageToTeamsWithBase64(imagePath, imageDetail) {
 }
 
 (async () => {
-    const localFilePath = "core.png"; // Path ของไฟล์ในเครื่อง
+    const localFilePath = "YOU_PATH_IMAGE"; // Path ของไฟล์ในเครื่อง
     const imageDetail = "ทดสอบส่งภาพแบบ Base64 จาก Node.js"; // รายละเอียดของภาพ
 
     // ส่งข้อความพร้อมภาพแบบ Base64 ไปยัง Microsoft Teams
